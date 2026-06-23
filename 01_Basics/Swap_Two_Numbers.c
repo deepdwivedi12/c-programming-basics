ab316c892b41ee8c42ed497d9b4ae81f283e82d0
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-06-23 15:13:49 +0530

    Add Swap_Two_Numbers.c to swap two integers

---
+++ 01_Basics/Swap_Two_Numbers.c
@@ -0,0 +1,14 @@
+//Swap two numbers using third variable
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20, temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("a = %d, b = %d", a, b);
+
+    return 0;
+}
